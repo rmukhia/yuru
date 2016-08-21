af57c973f45fb6d2dbe03944fa95aafe4972624d
rmukhia <raunak.mukhia@gmail.com>
2016-08-21 18:29:53 +0000

    Some optimization, color scheme and php setting.

---
+++ resources/assets/js/common.js
@@ -0,0 +1,25 @@
+$(function(){
+	$.material.init();
+	var $page = $("meta[name='page-name']").attr('content');
+	switch ($page) {
+		/* Homepage */
+		case 'home':
+			homeInit();
+			break;
+		/* Admin Page */
+		case 'admin-edit':
+			adminInit();
+			break;
+		/* Undefined default pages */
+		case undefined:
+			break;
+		/* All other pages */
+		default:
+			pageInit();
+	}
+});
+
+$(window).load(function() {
+    $("#loader-wrapper").hide();
+    $('html').css('opacity','1');
+});
